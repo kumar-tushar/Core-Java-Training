6e1b41bb1468ed48139e6e9c24ff0f43aeebd5e9
kumar-tushar <kumartushar0110@gmail.com>
2021-05-16 00:02:56 +0530

    16/05 12:02AM

--- Module_1/FirstApp.java
+++ Module_1/FirstApp.java
@@ -3,5 +3,8 @@ package Module_1;
 public class FirstApp {
     public static void main(String[] args) {
         System.out.println("Hi there!");
+        double GDP=2.454;
+        char currency='\u20B9';
+        System.out.println("India has GDP of "+ GDP+" trillion dollar and its currency symbol is " +currency);
     }
 }
