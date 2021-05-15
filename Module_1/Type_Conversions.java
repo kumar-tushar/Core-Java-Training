933dc4230c889680b741af9167deadb78c9195d6
kumar-tushar <kumartushar0110@gmail.com>
2021-05-16 04:57:30 +0530

    16/05

---
+++ Module_1/Type_Conversions.java
@@ -0,0 +1,16 @@
+package Module_1;
+
+public class Type_Conversions {
+
+    public static void main(String[] args) {
+
+        int radius=400;
+        double newRadius=radius;
+        System.out.println(newRadius);
+        
+        double diameter=3.78;
+        int newDiameter=(int)diameter;
+        System.out.println(newDiameter);
+
+    }
+}
