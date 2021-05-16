1e3da53673ae008ae43af64c56f1560ba27429bb
kumar-tushar <kumartushar0110@gmail.com>
2021-05-16 05:41:20 +0530

    16/05 12:02AM

---
+++ Module_1/Taking_Input.java
@@ -0,0 +1,28 @@
+package Module_1;
+
+import java.util.Scanner;
+
+public class Taking_Input {
+    public static void main(String[] args) {
+
+        String name;
+        int population;
+        float populationDensity;
+        double GDP;
+        char currency;
+        boolean isSecular;
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter country:");
+        name=scanner.nextLine();
+        System.out.println("Enter population:");
+        population= scanner.nextInt();
+        System.out.println("Currency:");
+        currency=scanner.next().charAt(0);
+        System.out.println("Country: "+name);
+        System.out.println("Population: "+population);
+        System.out.println("Currency: "+currency);
+        scanner.close();
+
+    }
+}
