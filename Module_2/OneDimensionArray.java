07039b866396afbaa69776bae599a39ffa372cc0
kumar-tushar <kumartushar0110@gmail.com>
2021-05-20 17:36:41 +0530

    20/05/21

--- Module_2/OneDimensionArray.java
+++ Module_2/OneDimensionArray.java
@@ -4,6 +4,21 @@ public class OneDimensionArray {
 
     public static void main(String[] args) {
 
+        int[] myFirstArray={20,19,48,7};
+
+        myFirstArray[2]=37;
+        System.out.println(myFirstArray[2]);
+
+        System.out.println();
+
+        System.out.println(myFirstArray.length);
+
+        System.out.println();
+
+        for(int i:myFirstArray){
+            System.out.println(i);
+        }
+
 
     }
 }
