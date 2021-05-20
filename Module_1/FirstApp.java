c1c462e5337ae33a152d2eff6338e2eb96c3c8d1
kumar-tushar <kumartushar0110@gmail.com>
2021-05-20 07:35:33 +0530

    20/05/21

--- Module_1/FirstApp.java
+++ Module_1/FirstApp.java
@@ -4,8 +4,8 @@ public class FirstApp {
 
     public static void main(String[] args) {
         System.out.println("Hi there!");
-        double GDP=2.454;
-        char currency='\u20B9';
-        System.out.println("India has GDP of "+ GDP+" trillion dollar and its currency symbol is " +currency);
+        double GDP = 2.454;
+        char currency = '\u20B9';
+        System.out.println("India has GDP of " + GDP + " trillion dollar and its currency symbol is " + currency);
     }
 }
---
+++ Module_2/ForLoop.java
@@ -0,0 +1,13 @@
+package Module_2;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        for(int num=1; num<=4; num++){
+            if(num%2!=0){
+                System.out.println(num);
+            }
+        }
+    }
+}
