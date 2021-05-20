bca18c570c583fa684bee43cc479262cc72b3fc3
kumar-tushar <kumartushar0110@gmail.com>
2021-05-20 11:23:46 +0530

    20/07/21

--- Module_2/DoWhileLoop.java
+++ Module_2/DoWhileLoop.java
@@ -1,13 +1,27 @@
 package Module_2;
 
+import java.util.Scanner;
+
 public class DoWhileLoop {
 
     public static void main(String[] args) {
 
-        for(int num=1; num<=4; num++){
-            if(num%2!=0){
-                System.out.println(num);
-            }
-        }
+        int count=0;
+        do{
+            System.out.println(count);
+            count++;
+        }while(count<=10);
+
+        System.out.println();
+
+        Scanner scanner=new Scanner(System.in);
+        double number;
+        double sum=0;
+        do{
+            System.out.println("Enter a Number: ");
+            number=scanner.nextDouble();
+            sum+=number;
+        }while(number!=0);
+        System.out.println(sum);
     }
 }
