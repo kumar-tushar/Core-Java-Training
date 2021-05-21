6db65f4f40c97063a6b7f8b49d650ec546a4aa26
kumar-tushar <kumartushar0110@gmail.com>
2021-05-21 08:11:05 +0530

    21/05/21

--- Module_2/TwoDimensionArray.java
+++ Module_2/TwoDimensionArray.java
@@ -4,6 +4,17 @@ public class TwoDimensionArray {
 
     public static void main(String[] args) {
 
+        int[][] myFirst2DArray={ {3,5, 1,9},
+                                 {10,15,3,0},
+                                 {1,11,31,90},
+                                 {2,51,1,9}};
+
+        for (int[] row : myFirst2DArray) {
+            for (int col : row) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
