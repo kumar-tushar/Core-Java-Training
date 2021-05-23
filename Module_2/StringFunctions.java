2287ffda5cb99c9555fcec86ad6abdc42d477016
kumar-tushar <kumartushar0110@gmail.com>
2021-05-23 16:29:29 +0530

    23/05/21

--- Module_2/StringFunctions.java
+++ Module_2/StringFunctions.java
@@ -4,6 +4,25 @@ public class StringFunctions {
 
     public static void main(String[] args){
 
+        char[] myArray={'w', 'e', 'l', 'c', 'o', 'm', 'e'};
+        System.out.println(myArray);
+        System.out.println(myArray.length);
+
+        System.out.println();
+
+        String firstString="welcome";
+        System.out.println(firstString);
+        System.out.println(firstString.length());
+
+        System.out.println();
+
+        System.out.println(firstString.charAt(0));
+
+        System.out.println();
+
+        System.out.println(firstString.toUpperCase());
+        System.out.println(firstString.toLowerCase());
+
 
 
     }
