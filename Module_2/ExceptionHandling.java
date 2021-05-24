14de34ff8778317abb0badffc3b4a6e996441de5
kumar-tushar <kumartushar0110@gmail.com>
2021-05-24 20:07:32 +0530

    24/05/21

---
+++ Module_2/ExceptionHandling.java
@@ -0,0 +1,18 @@
+package Module_2;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args){
+
+        System.out.println("Program Starts");
+
+        int[] myArray={3, 45, 8, 10};
+
+        System.out.println(myArray[1]);
+
+        System.out.println("Program Ends");
+
+    }
+}
--- Module_2/MethodOverloading.java
+++ Module_2/MethodOverloading.java
@@ -4,5 +4,26 @@ public class MethodOverloading {
 
     public static void main(String[] args){
 
+        add();
+        add(10, 45);
+        add(10.4f, 7.8f);
+
     }
+
+    public static void add(){
+        int sum=5+7;
+        System.out.println(sum);
+    }
+
+    public static void add(int a, int b){
+        int sum=a+b;
+        System.out.println(sum);
+    }
+
+    public static void add(float a, float b){
+        float sum=a+b;
+        System.out.println(sum);
+    }
+
+
 }
