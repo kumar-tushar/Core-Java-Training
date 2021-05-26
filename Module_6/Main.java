5f7065a1892387f773043e3a1b93be1697646cb8
kumar-tushar <kumartushar0110@gmail.com>
2021-05-26 19:34:26 +0530

    26/5/21

--- Module_6/Main.java
+++ Module_6/Main.java
@@ -2,7 +2,23 @@ package Module_6;
 
 public class Main {
     public static void main(String[] args){
-
-
+        Animal animal=new Dog();
+        animal.eat();
+        animal.run();
     }
+}
+
+abstract class Animal{
+    public void run(){
+        System.out.println("Animal Running");
+    }
+    abstract void eat();
+}
+
+class Dog extends Animal{
+    @Override
+    public void eat(){
+        System.out.println("Dog Eating");
+    }
+
 }
