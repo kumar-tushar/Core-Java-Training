c4a42f7296d4fe2ffc555289b16ea9b3a5d6f046
kumar-tushar <kumartushar0110@gmail.com>
2021-05-27 15:47:23 +0530

    27/05/21

--- Module_8/Main.java
+++ Module_8/Main.java
@@ -14,3 +14,26 @@ interface Remote{
     abstract public void powerOff();
     abstract public void PowerOn();
 }
+
+class Television implements Remote{
+
+    @Override
+    public void volumeUp() {
+
+    }
+
+    @Override
+    public void volumeDown() {
+
+    }
+
+    @Override
+    public void powerOff() {
+
+    }
+
+    @Override
+    public void PowerOn() {
+
+    }
+}
