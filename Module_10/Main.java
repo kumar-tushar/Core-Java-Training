8aebca5959c4ea5fc3a3acc065204874447a06ac
kumar-tushar <kumartushar0110@gmail.com>
2021-05-29 23:23:38 +0530

    29/05/21

--- Module_10/Main.java
+++ Module_10/Main.java
@@ -7,6 +7,13 @@ public class Main {
     public static void main(String[] args){
 
         List<String> nameList=new ArrayList<>();
+        nameList.add("Sam");
+        nameList.add("Rahul");
+        nameList.add("Monika");
+        nameList.add("Tony");
 
+        for(String name:nameList){
+            System.out.println(name);
+        }
     }
 }
