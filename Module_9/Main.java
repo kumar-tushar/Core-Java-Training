cbebd1ba13ac3fd17e458c2fababeb33f8c86ed7
kumar-tushar <kumartushar0110@gmail.com>
2021-05-29 19:28:26 +0530

    29/05/21

--- Module_9/Main.java
+++ Module_9/Main.java
@@ -8,12 +8,22 @@ public class Main {
     }
 }
 
-class Employee{
+class Employee {
     public String name;
     public static final String company="Oracle";
     public static int count=0;
 
     public static void myFirstStaticMethod(){
         System.out.println("My First Static Method");
+        System.out.println(count);
+        test();
     }
+
+    public static void test(){
+        System.out.println("Testing");
+    }
+
+
+
+
 }
