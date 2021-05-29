d99b686aa926c38f1afaa9c895542d2bc1db0ac5
kumar-tushar <kumartushar0110@gmail.com>
2021-05-29 23:18:50 +0530

    29/05/21

---
+++ Module_10/Main.java
@@ -0,0 +1,12 @@
+package Module_10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+
+        List<String> nameList=new ArrayList<>();
+
+    }
+}
