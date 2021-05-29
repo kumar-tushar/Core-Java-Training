e361c5fd2b3f30a1af92d24943a0f4a8f442b5d3
kumar-tushar <kumartushar0110@gmail.com>
2021-05-29 19:22:34 +0530

    29/05/21

--- Module_9/Main.java
+++ Module_9/Main.java
@@ -1,7 +1,15 @@
 package Module_9;
 
 public class Main {
-    public static void main(String args[]){
-
+    public static void main(String[] args){
+        Employee.count=20;
+        System.out.println(Employee.company);
+        System.out.println(Employee.count);
     }
+}
+
+class Employee{
+    public String name;
+    public static final String company="Oracle";
+    public static int count=0;
 }
