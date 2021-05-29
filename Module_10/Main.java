a0dc4dff27495fa5f0c3d494ede59217a4b38c12
kumar-tushar <kumartushar0110@gmail.com>
2021-05-29 23:53:06 +0530

    29/05/21

--- Module_10/Main.java
+++ Module_10/Main.java
@@ -12,8 +12,39 @@ public class Main {
         nameList.add("Monika");
         nameList.add("Tony");
 
+        nameList.remove(1);
+        nameList.add(1,"Peter");
+        nameList.set(1, "Rahul");
+
+        nameList.remove("Monika");
+
         for(String name:nameList){
             System.out.println(name);
         }
+
+        System.out.println();
+
+        Employee e1=new Employee("Albert");
+        Employee e2=new Employee("Alex");
+        Employee e3=new Employee("Neha");
+
+
+        List<Employee> employeeList=new ArrayList<>();
+        employeeList.add(e1);
+        employeeList.add(e2);
+        employeeList.add(e3);
+
+        for(Employee employee:employeeList){
+            System.out.println(employee.name);
+        }
+
+    }
+}
+
+
+class Employee{
+    String name;
+    public Employee(String name){
+        this.name=name;
     }
 }
