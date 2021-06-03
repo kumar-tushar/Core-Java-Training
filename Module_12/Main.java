4a3a92fc7b54c99033ccc176d39c1fb661020faf
kumar-tushar <kumartushar0110@gmail.com>
2021-06-03 12:26:30 +0530

    3/6/21

--- Module_12/Main.java
+++ Module_12/Main.java
@@ -1,9 +1,11 @@
 package Module_12;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
+
     public static void main(String[] args) {
 
         String data="I Love India";
@@ -14,11 +16,23 @@ public class Main {
             enterData.write(byteData);
             enterData.close();
         }
-            catch(IOException e ){
+        catch(IOException e ){
             System.out.println();
         }
 
 
+
+        try {
+            FileInputStream readData = new FileInputStream("file.txt");
+            int i;
+            while((i=readData.read())!=-1){
+                System.out.print((char) i);
+            }
+            readData.close();
+        }
+        catch(IOException e){
+            System.out.println();
+        }
 
     }
 }
